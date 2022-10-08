ee21f0652e6adf389ad15477d6bc6c3af93d6a52
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-10-08 18:24:30 +0300

    feat: develops button comp & edits storybook layout

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import Icon from '../Icon/Icon.vue';
+import './_button.scss';
 export interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'tertiary';
+  variant?: 'primary' | 'secondary';
   size?: 'small' | 'medium' | 'large';
   disabled?: boolean;
   loading?: boolean;
   icon?: string;
   iconPosition?: 'left' | 'right';
   label?: string;
+  block?: boolean;
 }
 const props = withDefaults(defineProps<ButtonProps>(), {
   variant: 'primary',
@@ -17,6 +20,7 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   icon: '',
   iconPosition: 'left',
   label: '',
+  block: false,
 });
 
 defineEmits(['click']);
@@ -24,20 +28,19 @@ defineEmits(['click']);
 const classes = computed(() => {
   return {
     btn: true,
-    [`btn-${props.variant}`]: true,
-    [`btn-${props.size}`]: true,
+    [`btn--${props.variant}`]: true,
+    [`btn--${props.size}`]: true,
+    [`btn--icon-${props.iconPosition}`]: props.icon,
+    'btn--block': props.block,
     'btn-disabled': props.disabled,
     'btn-loading': props.loading,
   };
 });
 </script>
 <template>
-  <button
-    class="button"
-    :class="classes"
-    :disabled="disabled"
-    @click="$emit('click')"
-  >
+  <button :class="classes" :disabled="disabled" @click="$emit('click')">
+    <Icon v-if="props.icon" class="btn__icon" :name="props.icon" />
     <slot />
+    <span class="btn__label" v-if="!$slots['default']">{{ props.label }}</span>
   </button>
 </template>
