ed480c0abf54f179fbf910b23d459b1526a32ea4
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-10-08 14:43:36 +0300

    WIP: storybook improvements & adds the button story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { withSource, transformSource } from "./withSource"
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {
+    argTypesRegex: "^on[A-Z].*"
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  viewMode: "docs",
+  layout: "centered",
+  docs: {
+    transformSource,
+  }
 }
+
+export const decorators = [
+  withSource
+]
